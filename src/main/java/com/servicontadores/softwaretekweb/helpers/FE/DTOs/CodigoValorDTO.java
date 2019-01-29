bcf71c14c025c847ce6196b772ad1e5a43dd207c
Yesid <yesidregis@gmail.com>
2019-01-29 09:42:23 -0500

    Cambios 29/01/2019

---
+++ src/main/java/com/servicontadores/softwaretekweb/helpers/FE/DTOs/CodigoValorDTO.java
@@ -0,0 +1,11 @@
+package com.servicontadores.softwaretekweb.helpers.FE.DTOs;
+
+import lombok.Data;
+
+@Data
+public class CodigoValorDTO {
+
+    private String Codigo;
+    private String Valor;
+
+}
---
+++ src/main/java/com/servicontadores/softwaretekweb/helpers/FE/DTOs/NombreValorDTO.java
@@ -0,0 +1,15 @@
+package com.servicontadores.softwaretekweb.helpers.FE.DTOs;
+
+import lombok.Data;
+
+/**
+ * Created by yesidregis
+ * DTO utilizado para representar elementos de tipo llave/valor
+ */
+@Data
+public class NombreValorDTO {
+
+    private String Nombre;
+    private String Valor;
+
+}
